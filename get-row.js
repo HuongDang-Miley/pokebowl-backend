f8894c811649ea35e174c31e6325f85a78c46259
Huong Dang <miley.dawn@codeimmersives.com>
2020-07-14 16:14:07 -0400

    homeowork

--- get-row.js
+++ get-row.js
@@ -4,20 +4,32 @@
 // Check it out to see its content and shape!
 const pokemon = require('./pokemon.js');
 
+// const getRow = function(str) {
+//   let result
+
+// Way 1: My solution
+//   for (const name of pokemon) {
+//     if (str.toLowerCase() === name[0]) {
+//       result = name 
+//       break;
+//     } else if (str !== name[0]) {
+//       result = null
+//     }
+//   }
+//   return result
+// } 
+
+// WAY 2: Colin's Solution
 const getRow = function(str) {
-  let result
 
   for (const name of pokemon) {
-    if (str.toLowerCase() === name[0]) {
-      result = name 
-      break;
-    } else if (str !== name[0]) {
-      result = null
+    if (name[0] === str) {
+      return name
     }
   }
-  return result
-} 
 
+  return null
+}
 
 // OUR code here.
 
