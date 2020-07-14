5ab6b766799357b8730fbdc18ed27a474a138b25
Huong Dang <miley.dawn@codeimmersives.com>
2020-07-14 14:56:25 -0400

    lab

--- get-row.js
+++ get-row.js
@@ -4,7 +4,19 @@
 // Check it out to see its content and shape!
 const pokemon = require('./pokemon.js');
 
+const getRow = function(str) {
+  let result
 
+  for (const name of pokemon) {
+    if (str.toLowerCase() === name[0]) {
+      result = name 
+      break;
+    } else if (str !== name[0]) {
+      result = null
+    }
+  }
+  return result
+} 
 
 
 // OUR code here.
--- get-url.js
+++ get-url.js
@@ -1,14 +1,17 @@
 // Your code here.
 // Note that you do NOT need the data for this function!
-
-
-
-
 // OUR code here.
 
+const getUrl = function(arr) {
+  if (arr === null) {
+    return null
+  } 
+  return arr[1]
+}
+
+// Test
 if (typeof getUrl === 'undefined') {
   getUrl = undefined;
 }
-
 
 module.exports = getUrl; 
